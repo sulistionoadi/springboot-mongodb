42b427b645f73d7682b249844ab875319372b3fb
Adi Sulistiono <sulistiono.adi@gmail.com>
2016-06-28 16:40:12 +0700

    interceptor ajaxRequest in angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,4 +39,28 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .service('APIInterceptor', function ($q, $location, $window) {
+      var service = this;
+
+      service.request = function (config) {
+        if(config.method === 'POST' || config.method === 'PUT') {
+          console.log(config);
+          var oHeader = {'alg': 'HS256', 'typ': 'JWT'};
+          var oPayload = {};
+
+          if(config.data) oPayload=config.data;
+
+          var sHeader = JSON.stringify(oHeader);
+          var sPayload = JSON.stringify(oPayload);
+          var sJWT = KJUR.jws.JWS.sign("HS256", sHeader, sPayload, "bismillah");
+
+          if(config.data) config.data={'payload':sJWT};
+        }
+        
+        return config;
+      };
+  })
+  .config(function ($httpProvider) {
+      $httpProvider.interceptors.push('APIInterceptor');
   });
